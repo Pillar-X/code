fdd74f01676329f20c1bd9379eb89cf03621ff2e
Pillar-X <pillar_x@163.com>
2024-11-19 20:13:00 +0800

    Test1

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            LoginFrame loginFrame = new LoginFrame(280,280);
+            LoginFrame loginFrame = new LoginFrame(1000,280);
             loginFrame.setVisible(true);
             LevelFrame levelFrame = new LevelFrame(500,200);
             levelFrame.setVisible(false);
